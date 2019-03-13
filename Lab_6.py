ee89e238473dc3af1a6a53ddf3dd9e11b7392a96
Mr.Khomutov <boss.khomutov@mail.ru>
2019-03-13 11:01:07 +0300

    Лаба сдана! Кластеризация монет сделана костыльно.

--- Lab_6.py
+++ Lab_6.py
@@ -8,17 +8,23 @@ __________
 |P8|P1|P4|
 |P7|P6|P5|
 """
+rho = 1
+thetta = 10 / 180
+threshold = 9
+minLineLength = 13
+maxLineGap = 20
 
 """ Функция сравнения двух одноканальных изображений, возвращает реультат совпадения в процентах"""
 def comparePict(img1, img2):
-    equal = 0
+    nonEqual = 0
     height, width = img1.shape
     diff = cv.subtract(img1, img2)
     for line in diff:
         for pixel in line:
-            if pixel == 0:
-                equal += 1
-    return (equal / (height * width)) * 100
+            if pixel != 0:
+                nonEqual += 1
+    return  nonEqual # В этом случае возвращает число несовпадающих пикселей
+    # return (equal / (height * width)) * 100
 
 
 """ Функция скелетизации изображения """
@@ -27,136 +33,130 @@ def comparePict(img1, img2):
 def skeletization(img):
     imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     rows, cols = imgGray.shape  # Размер изображения
-    image = cv.threshold(imgGray, 100, 1, type=cv.THRESH_BINARY)    # Бинаризация изображения
-    imgBin = np.copy(image[1])  # Массив для промежуточного хранения картинки
+    image1 = cv.inRange(imgGray, lowerb=100, upperb=255)
+
+    image1 = cv.dilate(image1, cv.getStructuringElement(cv.MORPH_ELLIPSE, (2, 3)), iterations=3)
+    image1 = cv.erode(image1, cv.getStructuringElement(cv.MORPH_ELLIPSE, (2, 3)), iterations=3)
+    image1 = cv.dilate(image1, cv.getStructuringElement(cv.MORPH_ELLIPSE, (2, 3)), iterations=3)
+    image1 = cv.erode(image1, cv.getStructuringElement(cv.MORPH_ELLIPSE, (2, 3)), iterations=3)
+
+    # cv.imshow('Binary', image1)
+    # cv.waitKey(0)
+    # cv.destroyWindow('Binary')
+
+    imgBin = np.copy(image1)  # Массив для промежуточного хранения картинки
     imgTemp = np.copy(imgBin)   # Временное изображение для текущей итерации скелетизации
 
-    #   -1 т.к. обработке не подвергаются граничные пиксели изображения
+    #   границы 1 и -1 т.к. обработке не подвергаются граничные пиксели изображения
+    operation = False
+    count = 0
     while True:
         for row in range(1, (rows - 1)):
             for col in range(1, (cols - 1)):
-                if imgBin[row][col] == 1:
+                # Если пиксель белый
+                if imgBin[row][col]:
+                    # Строим последовательность P2 P3 P4 P5 P6 P7 P8 P9
                     sequence = np.copy(
                         [imgBin[row - 1][col], imgBin[row - 1][col + 1], imgBin[row][col + 1], imgBin[row + 1][col + 1],
                          imgBin[row + 1][col], imgBin[row + 1][col - 1], imgBin[row][col - 1], imgBin[row - 1][col - 1]])
 
                     countWhitePix = sum(sequence)  # Количество белых пикселей вокруг центрального
+                    check = imgBin[row -1: row + 1][cols - 1: cols + 1]
 
-                    if 2 <= countWhitePix <= 6:
+                    if (2 * 255) <= countWhitePix <= (6 * 255):
                         """ количество найденных последовательностей 01 
-                       в последовательности P2, P3, P4, P5, P6, P7, P8, P9, P2 """
+                        в последовательности P2, P3, P4, P5, P6, P7, P8, P9, P2 """
                         countZeroToOne = 0  # Количество переходов 0->1 в последовательности из пикселей P2 P3 P4 P5 P6 P7 P8 P9 P2
                         for number in range(sequence.size):
                             prevPix = sequence[number - 1]
                             tempPix = sequence[number]
-                            if prevPix == 0 and tempPix == 1:
+                            if tempPix == 255 and prevPix == 0:
                                 countZeroToOne += 1
+
                         """ Должен существовать только один переход от нуля к единице """
                         if countZeroToOne == 1:
-                            # Удаляются все юго-восточные  и северо-западные угловые пиксели
-                            if (sequence[0] * sequence[2] * sequence[4]) == 0 and (
-                                    sequence[2] * sequence[4] * sequence[6]) == 0:
-                                imgTemp[row][col] = 0
+                            if operation == True:
+                                # Удаляются все юго-восточные  и северо-западные угловые пиксели
+                                if ((int(sequence[0]) * int(sequence[2]) * int(sequence[4])) == 0) and ((
+                                        int(sequence[2]) * int(sequence[4]) * int(sequence[6])) == 0):
 
+                                    imgTemp[row][col] = 0
+                            else:
                             # Удаляются все пиксели на северо-западной границе и юго-восточные угловые пиксели (коммент неверен наверн, да не суть)
-                            elif (sequence[0] * sequence[2] * sequence[6]) == 0 and (
-                                    sequence[0] * sequence[4] * sequence[6]) == 0:
-                                imgTemp[row][col] = 0
+                                if ((int(sequence[0]) * int(sequence[2]) * int(sequence[6])) == 0) and ((
+                                        int(sequence[0]) * int(sequence[4]) * int(sequence[6])) == 0):
 
-        diff = comparePict(imgBin, imgTemp) # возвращает 100 - полное совпадение, 0 - нет совпадений
+                                    imgTemp[row][col] = 0
 
+        diff = comparePict(imgBin, imgTemp)     # возвращает 100 - полное совпадение, 0 - нет совпадений
+        operation = not operation # Смена условия для скелетизации - чтоб скелетизация выполнялась равномерно
+
+        count += 1
+        print(count)
+        # cv.imshow('step', imgTemp)
+        # cv.waitKey(0)
         """ Скелетизация выполняется до тех пор, пока в новой итерации не будет удален ни один пиксель """
-        if diff == 100:
+        if diff == 0:
             # Домножение для того, чтоб можно было визуально представить результат
-            imgTemp *= 255
             imgTemp = cv.cvtColor(imgTemp, cv.COLOR_GRAY2BGR)
+            cv.imshow('skelet', imgTemp)
+            cv.imshow('original', img)
+            cv.waitKey(0)
+            cv.destroyWindow('skelet')
             return imgTemp
 
-        imgBin = imgTemp[:][:]
+        imgBin = np.copy(imgTemp[:][:])
 
 """ Функция соединения линий """
-def connectLines(skelet):
+""" На вход подается скелетизированное изображение, и оригинал - на котором отрисуются найденные линии """
+def connectLines(skelet, original):
     # 	Ищем линии
     skeletGray = cv.cvtColor(skelet, cv.COLOR_BGR2GRAY)
     lineImage = np.copy(skelet) * 0
 
-    skeletLines = cv.HoughLinesP(skeletGray, rho=0.1, theta=0.1, threshold=0, minLineLength=1)
+    skeletLines = cv.HoughLinesP(skeletGray, rho, thetta, threshold, minLineLength, maxLineGap=20)
 
-    new_lines = []
     points = []
-    max_dst = 50
-    angle_max = 10  # angle threshold on degrees
+    maxDist = 10
     for line in skeletLines:
-        if line[0, 1] < 250 or line[0, 3] < 250:
-            line[:] = 0, 0, 0, 0
-            print("pidor")
-        if line[0, 0]:
-            # c1x= (line[0,0]+line[0,2])//2
-            # c1y= (line[0,1]+line[0,3])//2
-            # line[0,0]=0
-            for second_line in skeletLines:
-                if second_line[0, 1] < 250 or second_line[0, 3] < 250:
-                    second_line[:] = 0, 0, 0, 0
-                    print("pidor")
-                if (second_line[0, 0] or second_line[0, 1]) and not np.allclose(line, second_line):
-                    dst1 = ((line[0, 0] - second_line[0, 0]) ** 2 + (
-                                line[0, 1] - second_line[0, 1]) ** 2) ** 0.5  # p11 p21
-                    dst2 = ((line[0, 0] - second_line[0, 2]) ** 2 + (
-                                line[0, 1] - second_line[0, 3]) ** 2) ** 0.5  # p11 p22
-                    dst3 = ((line[0, 2] - second_line[0, 0]) ** 2 + (
-                                line[0, 3] - second_line[0, 1]) ** 2) ** 0.5  # p12 p21
-                    dst4 = ((line[0, 2] - second_line[0, 2]) ** 2 + (
-                                line[0, 3] - second_line[0, 3]) ** 2) ** 0.5  # p12 p22
+        for second_line in skeletLines:
+            if (second_line[0, 0] or second_line[0, 1]) and not np.allclose(line, second_line):
+                dst1 = ((line[0, 0] - second_line[0, 0]) ** 2 + (
+                            line[0, 1] - second_line[0, 1]) ** 2) ** 0.5  # p11 p21
+                dst2 = ((line[0, 0] - second_line[0, 2]) ** 2 + (
+                            line[0, 1] - second_line[0, 3]) ** 2) ** 0.5  # p11 p22
+                dst3 = ((line[0, 2] - second_line[0, 0]) ** 2 + (
+                            line[0, 3] - second_line[0, 1]) ** 2) ** 0.5  # p12 p21
+                dst4 = ((line[0, 2] - second_line[0, 2]) ** 2 + (
+                            line[0, 3] - second_line[0, 3]) ** 2) ** 0.5  # p12 p22
 
-                    if dst1 < max_dst:  #
-                        points.append(
-                            ((line[0, 0] + 0.0, line[0, 1] + 0.0), (second_line[0, 0] + 0.0, second_line[0, 1] + 0.0)))
-                        print(" u1")
-                        cv.line(lineImage, (line[0, 0], line[0, 1]), (second_line[0, 0], second_line[0, 1]),
-                                 (255, 0, 0), 5)
-                        break
-                    if dst2 < max_dst:  #
-                        points.append(
-                            ((line[0, 0] + 0.0, line[0, 1] + 0.0), (second_line[0, 2] + 0.0, second_line[0, 3] + 0.0)))
-                        print("u2")
-                        cv.line(lineImage, (line[0, 0], line[0, 1]), (second_line[0, 2], second_line[0, 3]),
-                                 (255, 0, 0), 5)
-                        break
-                    if dst3 < max_dst:  #
-                        points.append(
-                            ((line[0, 2] + 0.0, line[0, 3] + 0.0), (second_line[0, 0] + 0.0, second_line[0, 1] + 0.0)))
-                        print(" u3")
-                        cv.line(lineImage, (line[0, 2], line[0, 3]), (second_line[0, 0], second_line[0, 1]),
-                                 (255, 0, 0), 5)
-                        break
-                    if dst4 < max_dst:  # line = (p12+p22)/2, (p11+p21)/2
-                        points.append(
-                            ((line[0, 2] + 0.0, line[0, 3] + 0.0), (second_line[0, 2] + 0.0, second_line[0, 3] + 0.0)))
-                        print(" u4")
-                        cv.line(lineImage, (line[0, 2], line[0, 3]), (second_line[0, 2], second_line[0, 3]),
-                                 (255, 0, 0), 5)
-                        break
+                if dst1 < maxDist:
+                    points.append(
+                        ((line[0, 0] + 0.0, line[0, 1] + 0.0), (second_line[0, 0] + 0.0, second_line[0, 1] + 0.0)))
+
+                    break
+                if dst2 < maxDist:  #
+                    points.append(
+                        ((line[0, 0] + 0.0, line[0, 1] + 0.0), (second_line[0, 2] + 0.0, second_line[0, 3] + 0.0)))
+
+                    break
+                if dst3 < maxDist:  #
+                    points.append(
+                        ((line[0, 2] + 0.0, line[0, 3] + 0.0), (second_line[0, 0] + 0.0, second_line[0, 1] + 0.0)))
+
+                    break
+                if dst4 < maxDist:  # line = (p12+p22)/2, (p11+p21)/2
+                    points.append(
+                        ((line[0, 2] + 0.0, line[0, 3] + 0.0), (second_line[0, 2] + 0.0, second_line[0, 3] + 0.0)))
+
+                    break
 
     for line in skeletLines:
         if line[0, 0] or line[0, 1]:
             for x1, y1, x2, y2 in line:
-                points.append(((x1 + 0.0, y1 + 0.0), (x2 + 0.0, y2 + 0.0)))
-                cv.line(lineImage, (x1, y1), (x2, y2), (255, 0, 0), 5)
+                cv.line(original, (x1, y1), (x2, y2), (0, 255, 0), 1)
 
-    for p in points:
-        x1 = int(p[0][0])
-        y1 = int(p[0][1])
-        x2 = int(p[1][0])
-        y2 = int(p[1][1])
-
-        cv.circle(lineImage, (x1, y1), 5, (100, 100, 100), -1)
-        cv.circle(lineImage, (x2, y2), 5, (100, 100, 100), -1)
-
-    lines_edges = cv.addWeighted(skelet, 0.8, lineImage, 1, 0)
-
-    cv.imwrite('path_lines2.png', lines_edges)
-    cv.imshow('tata', lines_edges)
-    cv.waitKey(0)
+    return lineImage
 
 
 """ Функция поиска и отрисовки линий на скелете изображения """
@@ -182,15 +182,27 @@ def drawSkeletLines(skelet):
 # Возвращает массив элементов, каждый элемент имеет структуру (х, y, r), х,у - центр круга, r - радиус
 def findCircles(img):
     imgTemp = np.copy(img)
+    imgTemp = cv.blur(imgTemp, (3, 3))
+    imgTemp = cv.blur(imgTemp, (3, 3))
+
     imgGray = cv.cvtColor(imgTemp, cv.COLOR_BGR2GRAY)
+    # grayilter = cv.threshold(imgGray, 160, 255, type=cv.THRESH_BINARY_INV)
+    # grayilter = cv.inRange(imgGray, 160, 255, cv.THRESH_BINARY_INV)
+    # cv.imshow('imgGray', imgGray)
+    # cv.waitKey(0)
+    #
+    # cv.imshow('origin', grayilter[1])
+    #
+    # cv.waitKey(0)
     # Поиск кругов на изображении
-    circles = cv.HoughCircles(imgGray, method=cv.HOUGH_GRADIENT, dp=1, minDist=20,
-                              param1=100,
-                              param2=60,
+    circles = cv.HoughCircles(imgGray, method=cv.HOUGH_GRADIENT, dp=1, minDist=17,
+                              param1=255,
+                              param2=61,
                               minRadius=3,
                               maxRadius=0)
 
     circles = np.uint16(np.around(circles))  # Округление чисел до целого
+    print(circles)
 
     return circles
 
@@ -263,7 +275,7 @@ def coinsMarking(coin1, coin2, coins):
         distToLatun = abs(averageCoin_2 - averageCircle)
 
         #  Если монетка ближе к никелевой - то помечается зеленой меткой, в обратном случае красной - латунная
-        if (distToNikel < distToLatun + 10):  # +10 - это костыль
+        if (distToNikel < distToLatun+40):  # +10 - это костыль
             cv.circle(coins, (circle[0], circle[1]), 3, (0, 255, 0), -1)
         else:
             cv.circle(coins, (circle[0], circle[1]), 3, (0, 0, 255), -1)
@@ -275,11 +287,11 @@ def coinsMarking(coin1, coin2, coins):
 
 
 # Первая часть задания
-letterA = cv.imread('A.jpg', cv.IMREAD_COLOR) # A.png - изображение буковки А из методички к лабе
-letterAGray = cv.cvtColor(letterA, cv.COLOR_BGR2GRAY)
+# letterA = cv.imread('A.jpg', cv.IMREAD_COLOR) # A.png - изображение буковки А из методички к лабе
+# letterAGray = cv.cvtColor(letterA, cv.COLOR_BGR2GRAY)
 
-skelet = skeletization(letterA)
-res = np.concatenate((skelet, letterA), axis=1)
+# skelet = skeletization(letterA)
+# res = np.concatenate((skelet, letterA), axis=1)
 
 """ Вывод скелетизированного изображения"""
 # cv.imshow('skelet', res)
@@ -288,21 +300,45 @@ res = np.concatenate((skelet, letterA), axis=1)
 
 
 # Вторая часть задания
-skeletLines = drawSkeletLines(skelet)
+# skeletLines = drawSkeletLines(skelet)
 
-cv.imshow('SkeletLines', skeletLines)
-cv.waitKey(0)
-cv.destroyWindow('SkeletLines')
+# cv.imshow('SkeletLines', skeletLines)
+# cv.waitKey(0)
+# cv.destroyWindow('SkeletLines')
 
-""" 
-Это не сделал как раз еще
+
+# Это не сделал как раз еще
 # Третья часть задания
-connectLines(skelet)
-skeletLines = drawSkeletLines(skelet)
-cv.imshow('SkeletLines', skelet)
-cv.waitKey(0)
-cv.destroyWindow('SkeletLines')
+# connectLines(skelet)
+# skeletLines = drawSkeletLines(skelet)
+
 """
+Функция за
+"""
+def getFrameFromVideo(filename, numberOfFrame):
+    video = cv.VideoCapture(filename)
+    if video.isOpened() is False:
+        print('Video is not opened.')
+
+    for i in range(numberOfFrame):
+        # Захват кадра из видеопотока
+        ret, frame = video.read()
+
+        if ret is False:
+            print("!!! Couldn't read frame!")
+            break
+
+        skelet = skeletization(frame)
+        skeletLinesConnected = connectLines(skelet, frame)
+
+        # cv.imshow('Skelet', skelet)
+        # cv.imshow('Skelet Lines COnnected', skeletLinesConnected)
+        cv.imshow('Frame Original', frame)
+        cv.waitKey(1)
+
+        print('Frame is processed')
+    cv.destroyWindow('Frame Original')
+    return frame
 
 # Четвертая часть задания
 coins_all = cv.imread('coins.jpg', cv.IMREAD_COLOR) # Загрузка изображения с монетами, взято из методички
@@ -314,3 +350,14 @@ markingCoins = coinsMarking(coin_1, coin_2, coins_all)
 cv.imshow('markingCoins', markingCoins)
 cv.waitKey(0)
 cv.destroyWindow('markingCoins')
+
+
+getFrameFromVideo('1.avi', 5)
+
+# cv.imshow('SkeletLines', skelet)
+# cv.waitKey(0)
+# cv.destroyWindow('SkeletLines')
+
+
+
+
